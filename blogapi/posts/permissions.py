c6eaa11b34592bddd119cbb39d667bf07ce2e7a1
Valentin Damache <valentin.damache@outlook.com>
2024-04-16 19:23:24 +0300

    implemented global and view permissions in posts app

--- blogapi/posts/permissions.py
+++ blogapi/posts/permissions.py
@@ -9,7 +9,7 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
         return False
 
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request so we'll always allow GET, HEAD or OPTIONS requests.
+        # Read permissions are allowed to any request so it will always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
             return True
 
