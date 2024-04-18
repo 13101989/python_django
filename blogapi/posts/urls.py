9039ad502607bc266c435a0162f5e07e3fcf6c23
Valentin Damache <valentin.damache@outlook.com>
2024-04-18 18:21:55 +0300

    added extra authorization on userviewset

--- blogapi/posts/urls.py
+++ blogapi/posts/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
-from .views import PostList, PostDetail
+# from .views import PostList, PostDetail, UserList, UserDetail
+from .views import PostViewSet, UserViewSet
 
-urlpatterns = [
-    path("", PostList.as_view(), name="post_list"),
-    path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
-]
+# urlpatterns = [
+#     path("", PostList.as_view(), name="post_list"),
+#     path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
+#     path("users/", UserList.as_view(), name="user_list"),
+#     path("users/<int:pk>/", UserDetail.as_view(), name="user_detail"),
+# ]
+
+router = SimpleRouter()
+router.register("users", UserViewSet, basename="users")
+router.register("", PostViewSet, basename="posts")
+
+
+urlpatterns = router.urls
